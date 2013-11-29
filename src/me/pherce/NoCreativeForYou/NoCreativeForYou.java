ed8b8ba8067c5515a1767ab8c6c7972eb2a5c6a2
pherce <blessedmyers@gmail.com>
2013-11-28 22:19:11 -0500

    Fixes by cjm721
    Added perms and cleaned up code.

--- src/me/pherce/NoCreativeForYou/NoCreativeForYou.java
+++ src/me/pherce/NoCreativeForYou/NoCreativeForYou.java
@@ -10,60 +10,55 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class NoCreativeForYou extends JavaPlugin implements Listener {
-	
-	final String BYPASS_PERM = "noCreativeForYou.bypass";
-	
-	@Override
-	public void onEnable() {
-		getServer().getPluginManager().registerEvents(this, this);
+        
+        final String BYPASS_PERM = "noCreativeForYou.bypass";
+        
+        @Override
+        public void onEnable() {
+                getServer().getPluginManager().registerEvents(this, this);
         System.out.println("NoCreativeForYou enabled");
     }
     
     @Override
-	public void onDisable() {
+        public void onDisable() {
         System.out.println("NoCreativeForYou disabled");
     }
    
     //Detects players switching from Survival Mode to Creative Mode and ban them if they're not op.
     @EventHandler
-	public void gameModeSwitch(PlayerGameModeChangeEvent event){
-    	Player player = (Player)event.getPlayer();
-<<<<<<< HEAD
-    	String banned = player.getDisplayName();
-		if(event.getNewGameMode()  == GameMode.CREATIVE && !player.isOp()){
-=======
-    	
-    	if(player.hasPermission(BYPASS_PERM) || player.isOp())
-    		return;
-    	
-		if(event.getNewGameMode() == GameMode.CREATIVE){
->>>>>>> a77434ed692a82a44d673211c995ab5a3ea822ec
-			event.setCancelled(true);
-			this.getServer().banIP(player.getAddress().getAddress().getHostAddress());
-			player.setBanned(true);
-        	player.kickPlayer("No hacking in Creative mode! NO SOUP FOR YOU!");
-        	
-        	this.getServer().broadcastMessage(ChatColor.RED.toString() + 
-        			player.getDisplayName() + " was banned for cheating in Creative Mode!");
+        public void gameModeSwitch(PlayerGameModeChangeEvent event){
+            Player player = (Player)event.getPlayer();
+            
+            if(player.hasPermission(BYPASS_PERM) || player.isOp())
+                    return;
+            
+                if(event.getNewGameMode() == GameMode.CREATIVE){
+                        event.setCancelled(true);
+                        this.getServer().banIP(player.getAddress().getAddress().getHostAddress());
+                        player.setBanned(true);
+                player.kickPlayer("No hacking in Creative mode! NO SOUP FOR YOU!");
+                
+                this.getServer().broadcastMessage(ChatColor.RED.toString() + 
+                                player.getDisplayName() + " was banned for cheating in Creative Mode!");
         }
     }
     
     //Detects Creative Mode on PlayerJoin and bans them if they're not op. Useful if players hacked Creative Mode before the server ran this plugin.
     @EventHandler
-	public void playerJoin(PlayerJoinEvent event){
-       	Player player = (Player)event.getPlayer();
-       	
-    	if(player.hasPermission(BYPASS_PERM) || player.isOp())
-    		return;
-       	
-		if(player.getGameMode() == GameMode.CREATIVE){
-			this.getServer().banIP(player.getAddress().getAddress().getHostAddress());
-			player.setBanned(true);
-        	player.kickPlayer("No hacking in Creative mode! NO SOUP FOR YOU!");
-        	
-        	this.getServer().broadcastMessage(ChatColor.RED.toString() + 
-        			player.getDisplayName() + " was banned for cheating in Creative Mode!");      	
-        	
+        public void playerJoin(PlayerJoinEvent event){
+               Player player = (Player)event.getPlayer();
+               
+            if(player.hasPermission(BYPASS_PERM) || player.isOp())
+                    return;
+               
+                if(player.getGameMode() == GameMode.CREATIVE){
+                        this.getServer().banIP(player.getAddress().getAddress().getHostAddress());
+                        player.setBanned(true);
+                player.kickPlayer("No hacking in Creative mode! NO SOUP FOR YOU!");
+                
+                this.getServer().broadcastMessage(ChatColor.RED.toString() + 
+                                player.getDisplayName() + " was banned for cheating in Creative Mode!");              
+                
         }
     }
 }
